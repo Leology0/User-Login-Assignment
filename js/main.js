255216f1d38638e8d99706298ccc2d22ae6fc221
Leology0 <112642025+Leology0@users.noreply.github.com>
2022-12-05 16:34:37 -0700

    changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // USER LOGIN / SIGNUP
 
+// Users Array
+let users = []
 // HTML VARIABLES
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
@@ -8,7 +10,20 @@ let signUpBtn = document.getElementById('sign-up-btn');
 signUpBtn.addEventListener('click', signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  // Create user by taking the inputs and pushing them into users array 
+  let username = document.getElementById("username-input").value;
+  let password = document.getElementById("password-input").value;
+  let confirmPassword = document.getElementById("confirm-password-input").value;
+  // Check if user already exists
+  for (let i = 0; i < users.length; i++) {
+    if (username == users[i].name) {
+      return alert("Username is already in use");
+    }
+  }
+  // Check if passwords match
+  
+  users.push(newUser(username, password))
+  alert("New user created")
 }
 
 // SIGN IN BTN CLICKED
@@ -16,4 +31,11 @@ signInBtn.addEventListener('click', signInHandler);
 
 function signInHandler() {
   console.log('Sign In Btn Clicked');
+}
+
+function newUser(username, password) {
+  return {
+    name: username,
+    pass: password,
+  }
 }
