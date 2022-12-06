3df29ff576d9a1ef9457eec0f8c1ed041d4b2ad9
Leology0 <leology42@gmail.com>
2022-12-06 08:54:53 -0700

    change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // USER LOGIN / SIGNUP
+let users = loadUsers()
 
 // HTML VARIABLES
 let signInBtn = document.getElementById('sign-in-btn');
@@ -8,8 +9,18 @@ let signUpBtn = document.getElementById('sign-up-btn');
 signUpBtn.addEventListener('click', signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
+  let nameInput = document.getElementById("username-Input").value;
+  let passwordInput = document.getElementById("password-Input").value;
+  let confirmPassword = document.getElementById("confirmPassword").value;
+  // check if user already exists and if password = confirm password
+  
+  // push user into the array
+  users.push(newUser(nameInput, passwordInput));
+  saveUsers();
+  alert("New user created");
+
 }
+
 
 // SIGN IN BTN CLICKED
 signInBtn.addEventListener('click', signInHandler);
@@ -17,3 +28,32 @@ signInBtn.addEventListener('click', signInHandler);
 function signInHandler() {
   console.log('Sign In Btn Clicked');
 }
+
+
+// Create new user
+function newUser(nameInput, passwordInput) {
+  return {
+    name: nameInput,
+    password: passwordInput
+  }
+}
+
+function checkUser (name) {
+  for(let i = 0; i < users.length; i++) {
+    if (name === users.name[i]) {
+      return index = -1
+    }
+  }
+}
+
+// Save Global users to Local Storage
+function saveUsers() {
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+// Load Tasks from Local Storage
+function loadUsers() {
+  let userstr = localStorage.getItem('users');
+  return JSON.parse(userstr) ?? [];
+}
+
